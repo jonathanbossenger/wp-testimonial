44974127a78ba686da531f29b35d23c5e8f9bd68
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-03-10 20:53:39 +0200

    Initial Commit

---
+++ wp-testimonial.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:     WP Testimonial
+ * Description:     Using a shortcode, render a testimonial anywhere on your site
+ * Version:         1.0.0
+ * Text Domain:     wp-testimonial
+ * License:         GPLv2 or later
+ *
+ * @package         WP_Testimonial
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'WPT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+add_shortcode( 'wp-testimonial', 'wpt_testimonial_shortcode' );
+function wpt_testimonial_shortcode( $attributes ) {
+	ob_start();
+	?>
+		<p style="font-weight: bold;"><?php echo $attributes['client'];?></p>
+		<p><?php echo $attributes['testimonial'];?></p>
+	<?php
+	return ob_get_clean();
+}
