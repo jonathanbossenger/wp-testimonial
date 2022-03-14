69564069a29cf6830de3948f36b0c5d258d72eda
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-03-14 11:31:18 +0200

    Moving inline style to external stylesheet

--- wp-testimonial.php
+++ wp-testimonial.php
@@ -15,12 +15,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 define( 'WPT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
+add_action('wp_enqueue_scripts', 'wpt_testimonial_shortcode_scripts');
+function wpt_testimonial_shortcode_scripts() {
+	wp_register_style('wpt-testimonial-style', WPT_PLUGIN_URL . 'css/style.css', array(), '1.0.0');
+
+}
+
 add_shortcode( 'wp-testimonial', 'wpt_testimonial_shortcode' );
 function wpt_testimonial_shortcode( $attributes ) {
+	wp_enqueue_style('wpt-testimonial-style');
 	ob_start();
 	?>
-		<p style="font-weight: bold;"><?php echo $attributes['client'];?></p>
-		<p><?php echo $attributes['testimonial'];?></p>
+		<div class="wp-testimonial-testimonial">
+			<p class="client"><?php echo $attributes['client']; ?></p>
+			<p><?php echo $attributes['testimonial']; ?></p>
+		</div>
 	<?php
 	return ob_get_clean();
 }
